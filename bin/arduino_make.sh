575c612b51ba09a72c42669a1a71685ee7d131ff
Ilya Dmitrichenko <ilya.dmitrichenko@cosm.com>
2013-02-15 14:51:07 +0000

    Make Arduino IDE path setteble

--- bin/arduino_make.sh
+++ bin/arduino_make.sh
@@ -2,10 +2,11 @@
 cd $(dirname $BASH_SOURCE)/../
 source ./env.sh
 
+ARDUINO_DIR=${ARDUINO_DIR:-"/Applications/Arduino-1.0.1.app/Contents/Resources/Java/"}
 RUN_ARDMK_SCRIPTS="${PERL} -I ${PERL_LOCAL_LIB_ROOT}/lib/perl5 ${ARDMK_DIR}/bin"
 
 $MAKE $@ \
-  ARDUINO_DIR="/Applications/Arduino-1.0.1.app/Contents/Resources/Java/" \
+  ARDUINO_DIR="${ARDUINO_DIR}" \
   ARDMK_DIR="${ARDMK_DIR}" \
   RESET_CMD="${RUN_ARDMK_SCRIPTS}/ard-reset-arduino" \
   PARSE_BOARD="${RUN_ARDMK_SCRIPTS}/ard-parse-boards" \
